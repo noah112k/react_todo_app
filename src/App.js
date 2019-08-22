eb4a9ddc76ae58fd036982e431ce7e2c6579f6f5
noah112k <50031627+noah112k@users.noreply.github.com>
2019-08-21 22:11:14 -0700

    Basic react setup, todo item components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Todos from "./components/Todos";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+
+class App extends React.Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: "Do the dishes",
+        completed: false
+      },
+      {
+        id: 2,
+        title: "Go to the grocery store",
+        completed: false
+      },
+      {
+        id: 3,
+        title: "Code React app",
+        completed: false
+      }
+    ]
+  };
+
+  markComplete = (id) => {
+    this.setState({todos: this.state.todos.map((todo) => {
+      if(todo.id == id) {
+        todo.completed = !todo.completed
+      }
+      return todo;
+    })})
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Todos todos={this.state.todos} markComplete = {this.markComplete}/>
+      </div>
+    );
+  }
 }
 
 export default App;
